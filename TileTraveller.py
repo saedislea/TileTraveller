53b5adc09756292c50510242374079c3bd6f8914
Sædís Lea <saedislea@Sdiss-MacBook-Pro.local>
2018-09-18 10:38:43 +0000

    bein leið komin

--- TileTraveller.py
+++ TileTraveller.py
@@ -12,3 +12,43 @@
     #3,1 -> Victory!
     #3,2 -> S, N
     #3,3 -> S, W
+
+n = "(N)orth"
+s = "(S)outh"
+w = "(W)est"
+e = "(E)ast"
+
+
+print("You can travel: ",n)
+direction = input("Direction: ")
+if direction == "n" or direction == "N":
+    #position = 1,2
+    print("You can travel :" + n + " or " + e + " or " + s + ".")
+    direction = input("Direction: ")
+    if direction == "n" or direction == "N":
+        #1,3
+        print("You can travel: " + e + " or " + s + ".")
+        direction = input("Direction: ")
+        if direction == "e" or direction == "E":
+            #2,3
+            print("You can travel :" + w + " or " + e + ".")
+            direction = input("Direction: ")
+            if direction == "e" or direction == "E":
+            #3,3
+                print("You can travel :" + s + " or " + "w" + ".")
+            direction = input("Direction: ")
+            if direction == "s" or direction == "S":
+            #3,2
+                print("You can travel: " + s + " or " + n + ".")
+            direction = input("Direction: ")
+            if direction == "s" or direction == "S":
+            #3,1
+                print("Victory!")
+    elif direction == "E" or direction == "e":
+        print("You can travel: " + s + " or " + w + ".")
+    elif direction == "s" or direction == "S":
+        print("You can travel :" + n + ".")
+    else:
+        print("Not a valid direction")
+else:
+    print("Not a valid direction")
